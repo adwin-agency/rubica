32fed45f9ee063bbe9afffc7089d509f5da137ec
Anton <anton@MacBook-Pro-platon.local>
2020-05-29 13:20:37 +0300

    first pushh dev-anton branch

--- src/js/main.js
+++ src/js/main.js
@@ -2,32 +2,33 @@
 //= ../../bower_components/owl.carousel/dist/owl.carousel.js
 //= ../../bower_components/fullpage.js/dist/fullpage.js
 //= ../../bower_components/swiper/package/js/swiper.min.js
-
+//= ../../bower_components/magnific-popup/dist/jquery.magnific-popup.min.js
 
 
 "use strict";
 
 //= partials/lazyload.js
 /*вертикальный скролл на главной*/
-var mySwiper = new Swiper ('.swiper-container', {
+var mySwiper = new Swiper('.swiper-container', {
     // Optional parameters
     direction: 'vertical',
     loop: false,
     speed: 1000,
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
-      bulletActiveClass: 'bullet-active',
-      bulletClass: 'swiper-bullet',
-      renderBullet: function (index, className) {
-        return '<span class="'+ className +'">' + (index+1) +'</span>'+ "<span class='bullet-line'></span>";},
+        el: '.swiper-pagination',
+        bulletActiveClass: 'bullet-active',
+        bulletClass: 'swiper-bullet',
+        renderBullet: function(index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>' + "<span class='bullet-line'></span>";
+        },
     },
 
     mousewheel: {
         invert: false,
-      },
+    },
 
-  });
+});
 
 
 function owlCarouselSlider(owlSlider) {
@@ -44,7 +45,7 @@ function owlCarouselSlider(owlSlider) {
             slideBy: 1,
             items: 1,
             lazyLoad: true,
-            onInitialized : pagen,
+            onInitialized: pagen,
             onChange: counter,
             responsive: {
                 310: {
@@ -73,24 +74,24 @@ function owlCarouselSlider(owlSlider) {
             }
         });
 
-    function pagen(event){
-            
-            $('.owl-prev').after("<div class='slider-pagen d-flex'>1/" + event.item.count+"</div>");
-            
-    }
+        function pagen(event) {
 
-    function counter(event) {
-        
-         var items     = event.item.count;     // Number of items
-         var item      = event.item.index;     // Position of the current item
-       
-       // it loop is true then reset counter from 1
-       if(item > items) {
-        item = item - items;
-      }
-       $('.slider-pagen').html(item+"/"+items);
-     }
-    
+            $('.owl-prev').after("<div class='slider-pagen d-flex'>1/" + event.item.count + "</div>");
+
+        }
+
+        function counter(event) {
+
+            var items = event.item.count; // Number of items
+            var item = event.item.index; // Position of the current item
+
+            // it loop is true then reset counter from 1
+            if (item > items) {
+                item = item - items;
+            }
+            $('.slider-pagen').html(item + "/" + items);
+        }
+
     }
 
 }
@@ -183,19 +184,20 @@ function owlCarouselSliderGoods(owlSlider) {
     }
 
 }
+
 function owlCarouselSliderideas(owlSlider) {
 
     if ($(owlSlider).length > 0) {
 
         $(owlSlider).owlCarousel({
             center: true,
-            items:2,
-            loop:true,
+            items: 2,
+            loop: true,
             dots: false,
-            margin:20,
-            responsive:{
-                768:{
-                    items:2
+            margin: 20,
+            responsive: {
+                768: {
+                    items: 2
                 }
             }
         });
@@ -203,17 +205,6 @@ function owlCarouselSliderideas(owlSlider) {
     }
 
 }
-$(document).ready(function(){
-    $(window).on('resize', function(){
-     if($(window).width() <= 768) {
-     $('.ideas__lockers-list').addClass('ideas-lockers-js');
-     }
-      else {
-     $('.ideas__lockers-list').removeClass('ideas-lockers-js');
-     }
-     }).trigger('resize');
-   });
-
 
 function owlCarouselSliderProduct(owlSlider) {
 
@@ -228,11 +219,8 @@ function owlCarouselSliderProduct(owlSlider) {
             slideBy: 1,
             items: 1,
             lazyLoad: true,
-            onInitialized : pagen,
+            onInitialized: pagen,
             onChange: counter,
-            
-         
-
             responsive: {
                 310: {
                     items: 1
@@ -261,23 +249,20 @@ function owlCarouselSliderProduct(owlSlider) {
         });
 
 
-        function pagen(event){
-            
-                $('.owl-prev').after("<div class='slider-pagen d-flex'>1/" + event.item.count+"</div>")
-                
+        function pagen(event) {
+            $('.owl-prev').after("<div class='slider-pagen d-flex'>1/" + event.item.count + "</div>")
         }
 
         function counter(event) {
-            
-             var items     = event.item.count;     // Number of items
-             var item      = event.item.index;     // Position of the current item
-           
-           // it loop is true then reset counter from 1
-           if(item > items) {
-            item = item - items;
-          }
-           $('.slider-pagen').html(item+"/"+items);
-         }
+
+            var items = event.item.count;
+            var item = event.item.index;
+
+            if (item > items) {
+                item = item - items;
+            }
+            $('.slider-pagen').html(item + "/" + items);
+        }
     }
 
 }
@@ -359,73 +344,318 @@ function inputmask() {
 
 }
 
+function fixedScrollElement() {
+    var $window = $(window),
+        $target = $("#fixed-scroll-js"),
+        $bottom = $('#end-scroll-js'),
+        $top = undefined;
+
+    if ($target && $bottom) {
+
+        var $top = $target.offset().top,
+            $height = $target.outerHeight(),
+            $left = $('.product').offset().left + $('.product').outerWidth(),
+            $bot = $bottom.offset().top + $bottom.outerHeight();
+
+        $window.on('scroll', function() {
+
+            var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+            if ($top != undefined) {
+
+                if (scrollTop > $top && scrollTop + $height < $bot) {
+
+                    $target.css({
+                        position: 'fixed',
+                        top: '10px',
+                        left: $left + 'px',
+                    });
+                } else if (scrollTop > $top && scrollTop + $height > $bot) {
+
+                    // Координата верха: куда нельзя заходить - минус верх - минус высота блока
+                    var top = $bot - scrollTop - $height;
+
+                    $target.css({
+                        top: top,
+                    });
+                }
+
+                // Иначе возвращаем всё назад
+                else {
+
+                    $target.css({
+                        position: 'relative',
+                        top: '',
+                        left: '',
+                    });
+                }
+            }
+
+        });
+    }
+}
+
+function responsiveZoom(item) {
+    var resFlag;
+    var zoom = '.zoom-card';
+
+    if (item === zoom && $(window).width() < 900) {
+
+        $(item).find('.slider__item').addClass('pointer-none');
+
+        resFlag = false;
+    } else {
+        $(item).find('.slider__item').removeClass('pointer-none');
+
+        resFlag = true;
+    }
+
+    return resFlag;
+}
+
+function zoomPopUp(item, elementClick) {
+
+    var flag = responsiveZoom(item);
+
+    $(item).each(function() {
+
+        $(this).magnificPopup({
+            delegate: elementClick,
+            type: 'image',
+            closeOnContentClick: false,
+            closeBtnInside: false,
+            disableOn: function() {
+                return flag;
+            },
+            mainClass: 'mfp-with-zoom mfp-img-mobile',
+            callbacks: {
+                beforeOpen: function() {
+                    this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+                    this.st.mainClass = this.st.el.attr('data-effect');
+                }
+            },
+            image: {
+                verticalFit: true
+            },
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0, 1],
+                tCounter: '<span class="mfp-counter">%curr% из %total%</span>'
+            },
+            zoom: {
+                enabled: true,
+                duration: 300,
+                opener: function(element) {
+                    return element.find('img');
+                }
+            }
+
+        });
+
+    });
+
+}
+
+function owlSliderWithThumbs(slider, thumbs) {
+
+    var carouselMain = $(slider);
+    var carouselSecond = $(thumbs);
+    var slidesPerPage = 4;
+    var syncedSecondary = true;
+
+    carouselMain.owlCarousel({
+        items: 1,
+        loop: true,
+        dots: false,
+        autoplay: false,
+        nav: true,
+        lazyLoad: true,
+        animateIn: 'scaleUpFrom',
+        animateOut: 'scaleUpTo',
+        transitionStyle: 'fade',
+        onInitialized: pagen,
+        responsiveRefreshRate: 200,
+        navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 3px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 3px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>']
+    }).on('changed.owl.carousel', syncPosition);
+
+    function pagen(event) {
+        $('.owl-prev').after("<div class='slider-pagen d-flex'>1/" + event.item.count + "</div>");
+    }
+
+    if (carouselSecond.length > 0) {
+        carouselSecond
+            .on('initialized.owl.carousel', function() {
+                carouselSecond.find(".owl-item").eq(0).addClass("current");
+            })
+            .owlCarousel({
+                items: slidesPerPage,
+                dots: false,
+                nav: true,
+                margin: 20,
+                smartSpeed: 200,
+                slideSpeed: 500,
+                slideBy: slidesPerPage,
+                lazyLoad: true,
+                responsiveRefreshRate: 100
+            }).on('changed.owl.carousel', syncPosition2);
+    }
+
+    function syncPosition(el) {
+
+        var count = el.item.count - 1;
+        var current = Math.round(el.item.index - (el.item.count / 2) - .5);
+
+        var items = el.item.count;
+        var item = Math.round(el.item.index - (el.item.count / 2) - .5);;
+
+        if (item > items) {
+            item = item - items;
+        }
+
+        $('.slider-pagen').html(item + "/" + items);
+
+        if (current < 0) {
+            current = count;
+            item = items;
+        }
+        if (current > count) {
+            current = 0;
+            item = 0;
+        }
+
+        if (carouselSecond.length > 0) {
+            carouselSecond
+                .find(".owl-item")
+                .removeClass("current")
+                .eq(current)
+                .addClass("current");
+            var onscreen = carouselSecond.find('.owl-item.active').length - 1;
+            var start = carouselSecond.find('.owl-item.active').first().index();
+            var end = carouselSecond.find('.owl-item.active').last().index();
+
+            if (current > end) {
+                carouselSecond.data('owl.carousel').to(current, 100, true);
+            }
+            if (current < start) {
+                carouselSecond.data('owl.carousel').to(current - onscreen, 100, true);
+            }
+        }
+
+
+    }
+
+    function syncPosition2(el) {
+        if (syncedSecondary) {
+            var number = el.item.index;
+            carouselMain.data('owl.carousel').to(number, 100, true);
+        }
+    }
+
+    carouselSecond.on("click", ".owl-item", function(e) {
+        e.preventDefault();
+        var number = $(this).index();
+        carouselMain.data('owl.carousel').to(number, 300, true);
+    });
+
+}
+
+function initScroll(item, block) {
+
+    var a = document.querySelector(item),
+        b = null,
+        P = 100;
+
+    if (a) {
+        if ($(window).width() >= 1279) {
+
+            window.addEventListener('scroll', Ascroll, false);
+            document.body.addEventListener('scroll',Ascroll , false);
+
+            function Ascroll() {
+                if (b == null) {
+                    var Sa = getComputedStyle(a, ''),
+                        s = '';
+                    for (var i = 0; i < Sa.length; i++) {
+                        if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
+                            s += Sa[i] + ': ' + Sa.getPropertyValue(Sa[i]) + '; '
+                        }
+                    }
+                    b = document.createElement('div');
+                    b.classList.add('fixed-div');
+                    b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
+                    a.insertBefore(b, a.firstChild);
+                    var l = a.childNodes.length;
+                    for (var i = 1; i < l; i++) {
+                        b.appendChild(a.childNodes[1]);
+                    }
+                    a.style.height = b.getBoundingClientRect().height + 'px';
+                    a.style.padding = '0';
+                    a.style.border = '0';
+                }
+                var Ra = a.getBoundingClientRect(),
+                    R = Math.round(Ra.top + b.getBoundingClientRect().height - document.querySelector(block).getBoundingClientRect().bottom); // селектор блока, при достижении нижнего края которого нужно открепить прилипающий элемент
+                if ((Ra.top - P) <= 0) {
+                    if ((Ra.top - P) <= R) {
+                        b.className = 'stop';
+                        b.style.top = -R + 'px';
+                    } else {
+                        b.className = 'sticky';
+                        b.style.top = P + 'px';
+                    }
+                } else {
+                    b.className = '';
+                    b.style.top = '';
+                }
+                window.addEventListener('resize', function() {
+                    a.children[0].style.width = getComputedStyle(a, '').width
+                }, false);
+            }
+
+        }
+        else{
+            document.querySelector('.product-info').classList.add('static');
+
+        }
+    }
+
+}
+
+
+
+$(window).on('resize', function() {
+    if ($(window).width() <= 768) {
+        $('.ideas__lockers-list').addClass('ideas-lockers-js');
+    } else {
+        $('.ideas__lockers-list').removeClass('ideas-lockers-js');
+    }
+
+    if ($(window).width() <= 1279) {
+        var p = $('.product-info').detach();
+        $(".product-main ").prepend(p);
+    }
+    else{
+        var p = $('.product-info').detach();
+        $(".main > .product-container ").append(p);
+    }
+
+    initScroll();
+
+}).trigger('resize');
+
+
 
 
 $(function() {
-
+    // fixedScrollElement();
+    initScroll('.product-info', '.product');
     owlCarouselSlider('.promo-carousel-js');
     owlCarouselSliderProd('.prod-slider__carousel-js');
     owlCarouselSliderGoods('.goods__item-slider-js');
-    owlCarouselSliderProduct('.product__item-slider-js');
+    // owlCarouselSliderProduct('.product__item-slider-js');
     owlCarouselSliderGoods('.ideas-slider-js');
     owlCarouselSliderideas('.ideas-lockers-js');
-    
-   
+    zoomPopUp('.zoom-card-js', '.product__slider-list-item');
+    owlSliderWithThumbs('.product__item-slider-js', '.product__carusel-list');
 
 });
-
-
-jQuery( document ).ready(function( $ ) {
-    var
-   
-      $window = $( window ), 
- 
-      $target = $( "#fixed-scroll-js" ),
- 
-      $bottom = $( '#end-scroll-js' ), 
- 
-      $top = $target.offset().top, 
- 
-      $height = $target.outerHeight(), 
-
-        $left = $('.product').offset().left + $('.product').outerWidth(),
-
-       $bot = $bottom.offset().top + $bottom.outerHeight();
-       
-   
-    $window.on('scroll', function() {
-   
-      var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-      if (scrollTop > $top  && scrollTop + $height < $bot) {
-   
-        
-        $target.css( {     
-            position : 'fixed',
-            top : '10px',     
-            left : $left + 'px',
-        } );
-    } 
-
-        else if ( scrollTop > $top && scrollTop + $height > $bot ) {
- 
-            // Координата верха: куда нельзя заходить - минус верх - минус высота блока
-            var top = $bot - scrollTop - $height;
- 
-            $target.css( {
-                
-                top : top ,
-            } );
-        }
-   
-      // Иначе возвращаем всё назад
-      else {     
-   
-        $target.css( {     
-            position : 'relative',
-            top : '',     
-            left : '',
-        } );
-      }
-    });
-});   
